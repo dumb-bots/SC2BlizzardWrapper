a5893b5adc85efa0248c20fa8ad539160cb967e4
marcelo <marcelo@marcelo.marcelo>
2018-05-27 18:00:04 -0300

    load from replay

--- players/random.py
+++ players/random.py
@@ -4,25 +4,17 @@ from api_wrapper.player import Player
 from constants.ability_ids import AbilityId
 class RandomPlayer(Player):
     async def process_step(self, ws, game_state, actions=None):
-        significant = False
-        counter = 0
-        if game_state.game_loop > 1500:
-            while not significant and counter < 10:
-                index = random.randint(0, len(actions) - 1)
-                action = actions[index]
-                manager = UnitManager([action.unit])
-                if action.unit.name != "SCV" or (action.ability_id > 30):
-                    significant = True
-                counter += 1
-                
-            if action.require_target:
-                x = random.randint(-10,10)
-                y = random.randint(-10,10)
-                unit_x = action.unit.get_attribute("pos").x
-                unit_y = action.unit.get_attribute("pos").y
-                await  manager.give_order(ws, action.ability_id, target_point=(unit_x + x,unit_y + y))
-            else:
-                await manager.give_order(ws, action.ability_id)
+        index = random.randint(0, len(actions) - 1)
+        action = actions[index]
+        manager = UnitManager([action.unit])    
+        if action.require_target:
+            x = random.randint(-10,10)
+            y = random.randint(-10,10)
+            unit_x = action.unit.get_attribute("pos").x
+            unit_y = action.unit.get_attribute("pos").y
+            await  manager.give_order(ws, action.ability_id, target_point=(unit_x + x,unit_y + y))
+        else:
+            await manager.give_order(ws, action.ability_id)
 
 
         
