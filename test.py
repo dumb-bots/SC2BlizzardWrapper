379c07d837c8bba1cb3d6894aafc91b54b1025e1
Marcelo <marcelosiccardi@gmail.com>
2018-12-09 16:43:31 -0300

    Test action player

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from os import listdir
 from os.path import isfile, join
 from multiprocessing import Pool
 from subprocess import call
+
+from players.actions import ActionsPlayer, DEMO_ACTIONS_9
 from players.cbr_player import CBRPlayer
 from pympler import tracker
 
@@ -43,38 +45,44 @@ loop = asyncio.get_event_loop()
 #     loop.run_until_complete(
 #         ia_vs_ia("Ladder2017Season3/InterloperLE.SC2Map", "Terran", "VeryHard", 24)
 #     )
-while True:
-    player1 = CBRPlayer()
-    player2 = CBRPlayer()
-    loop.run_until_complete(
-        player2.create(
-            "Terran",
-            "Human",
-            500,
-            "Terran",
-            DATABASE_NAME,
-            DATABASE_ROUTE,
-            DATABASE_PORT,
-            server_route=SERVER_ROUTE,
-            server_address=SERVER_ADDRESS,
-        )
-    )
-    loop.run_until_complete(
-        player1.create(
-            "Terran",
-            "Human",
-            500,
-            "Terran",
-            DATABASE_NAME,
-            DATABASE_ROUTE,
-            DATABASE_PORT,
-            server_route=SERVER_ROUTE,
-            server_address=SERVER_ADDRESS,
-        )
-    )
-    loop.run_until_complete(
-        player_vs_player(player1, player2, "Ladder2017Season3/InterloperLE.SC2Map", 24)
-    )
+
+player1 = ActionsPlayer()
+player_args = {'race': "Terran", 'obj_type': "Human", 'server_route': SERVER_ROUTE, 'server_address': SERVER_ADDRESS,
+               'actions': DEMO_ACTIONS_9}
+loop.run_until_complete(play_vs_ia(player1, player_args, "Ladder2017Season3/InterloperLE.SC2Map", "Zerg", "VeryEasy", 100))
+
+# while True:
+#     player1 = CBRPlayer()
+#     player2 = CBRPlayer()
+#     loop.run_until_complete(
+#         player2.create(
+#             "Terran",
+#             "Human",
+#             500,
+#             "Terran",
+#             DATABASE_NAME,
+#             DATABASE_ROUTE,
+#             DATABASE_PORT,
+#             server_route=SERVER_ROUTE,
+#             server_address=SERVER_ADDRESS,
+#         )
+#     )
+#     loop.run_until_complete(
+#         player1.create(
+#             "Terran",
+#             "Human",
+#             500,
+#             "Terran",
+#             DATABASE_NAME,
+#             DATABASE_ROUTE,
+#             DATABASE_PORT,
+#             server_route=SERVER_ROUTE,
+#             server_address=SERVER_ADDRESS,
+#         )
+#     )
+#     loop.run_until_complete(
+#         player_vs_player(player1, player2, "Ladder2017Season3/InterloperLE.SC2Map", 24)
+#     )
 # loop.run_until_complete(player2.create(
 #     "Terran", "Human", server_route=SERVER_ROUTE, server_address=SERVER_ADDRESS))
 # loop.run_until_complete(player_vs_player(
