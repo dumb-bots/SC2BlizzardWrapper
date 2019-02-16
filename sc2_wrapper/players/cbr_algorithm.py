9cb109e4b35df6220bf9a8d33eac15b3fdbb7e37
marcelo-oktana <marcelom@oktana.io>
2019-02-16 09:21:28 -0300

    typo

--- sc2_wrapper/players/cbr_algorithm.py
+++ sc2_wrapper/players/cbr_algorithm.py
@@ -72,7 +72,7 @@ class CBRAlgorithm(RulesPlayer):
                     for action in actions:
                         list_of_actions.append([action, self.evaluate_action(action)])
                     list_of_actions = list(map(lambda x: [x[0], x[1]/ selected_case["games"]], list_of_actions))
-                    maximum_fitness = max(map(lambda x: x[1], items))
+                    maximum_fitness = max(map(lambda x: x[1], list_of_actions))
                     list_of_actions = list(map(lambda x: [x[0], x[1] / maximum_fitness], list_of_actions))
                     print(list_of_actions)
                     for action in list_of_actions:
