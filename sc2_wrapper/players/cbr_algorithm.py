e3b12f9f3f0ce6982a9c55d2930c21f765ddcc56
marcelo-oktana <marcelom@oktana.io>
2019-02-18 10:47:25 -0300

    big cases fix

--- sc2_wrapper/players/cbr_algorithm.py
+++ sc2_wrapper/players/cbr_algorithm.py
@@ -49,7 +49,7 @@ class CBRAlgorithm(RulesPlayer):
             look_cases = self.cases_by_loop.get(round(situation["loop"] / float(200)), [])
             if not look_cases and situation["loop"] > 40000:
                 keys = sorted(self.cases_by_loop.keys())[-10:]
-                look_cases = look_cases[keys[random.randint(0,9)]]
+                look_cases = self.cases_by_loop[keys[random.randint(0,9)]]
             if not look_cases:
                 return actions
             for case in look_cases:
